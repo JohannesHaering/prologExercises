af59b36c8ea47eff78647e62274efacc85b90931
Johannes Haering <johannes.haering99@gmx.de>
2021-03-11 16:18:43 +0100

    First small prolog programm

---
+++ exercise1_4.pl
@@ -0,0 +1,8 @@
+killer(butch).
+married(mia, marsellus).
+dead(zed).
+kills(marsellus, footMassage(X, mia)).
+loves(mia, goodDancer(X)).
+eats(jules, X) :- nutrious(X), tasty(X).
+nutrious(brocoli).
+tasty(brocoli).
